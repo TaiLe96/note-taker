135a58790d6e05c6e2f2386ddff86a5700ec1645
Tai Le <tle449@gmail.com>
2020-01-13 17:08:10 -0800

    added db/dbjson

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var path = require("path");
-
+var fs = require("fs");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,6 +20,16 @@ app.get("/notes", function(req, res){
 
 // Get API notes and should read the db.json file and return all saved notes as JSON
 app.get("/api/notes", function(req, res){
+    fs.readFile('./db/db.json',function(err,content){
+        if(err) throw err;
+        var parseJson = JSON.parse(content);
+        for (i=0; i < notes.length ; i++){
+         parseJson.notes.push(newNotes)
+        }
+        fs.writeFile('./db/dbjson',JSON.stringify(parseJson),function(err){
+          if(err) throw err;
+        })
+      })
     return res.json(notes)
 });
 
@@ -29,6 +39,41 @@ app.post("/api/notes", function(req, res){
     notes.push(newNotes);
     res.json(newNotes);
 });
+
+
+
+
+// To push notes to db.json
+// app.get("/api/notes", function(req, res){
+//     fs.readFile(path.join(__dirname, "./db/db.json"))
+// });
+
+// app.post('/notes', function(req, res){
+//     var body = "";
+//     req.on('data', function(chunk){
+//       body += chunk;
+//     });
+//     req.on('end', function(){
+//       fs.readFile(__dirname + './db/db.json', function(err, data){
+//         if (err) throw err;
+//         console.log(body);
+//         var fileObj = JSON.parse(data.toString());
+//         var postObj = JSON.parse(body);
+//         fileObj.notes.push(postObj);
+//         var returnjson = JSON.stringify(notes);
+//         fs.writeFile(__dirname + './db/db.json', returnjson, function(err){
+//           if (err) throw err;
+//           res.send(returnjson);
+//         });
+//       });
+//     });
+//   });
+
+
+
+
+
+
 
 // Route to home page (index.html)
 app.get("*", function(req, res){
